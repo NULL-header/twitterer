bc96f7731bd18fe4b6634e974f9ae0af4fe085f8
NULL-header <poaoq.poaoq@gmail.com>
2020-02-17 12:35:12 +0900

    add discord token into .env

--- src/app.py
+++ src/app.py
@@ -34,4 +34,35 @@ bindlogger.addHandler(handler)
 
 logger.debug("set up.")
 
-print(setting.DISCORD_WEBHOOK)
+
+def keys():
+    return {
+        "DISCORD_TOKEN": setting.DISCORD_TOKEN,
+        "DISCORD_WEBHOOK": setting.DISCORD_WEBHOOK,
+        "CONSUMER_KEY": setting.CONSUMER_KEY,
+        "CONSUMER_SERCRET": setting.CONSUMER_SERCRET,
+        "ACCESS_TOKEN": setting.ACCESS_TOKEN,
+        "ACCESS_TOKEN_SERCRET": setting.ACCESS_TOKEN_SERCRET,
+    }
+
+
+def main():
+    args = sys.argv
+    if len(args) == 1:
+        logger.debug("args have no items.")
+        prfix = "!DEFAULT!"
+    else:
+        logger.debug("args have items, {}.".format(args[1]))
+        prfix = args[1]
+    try:
+        from bot import Bot
+        bot = Bot(keys(), prfix=prfix)
+        logger.debug("bot run.")
+        bot.wake()
+    except Exception:
+        logger.error("could not read token;\n\n"
+                     "with this traceback:{0}\n".format(sys.exc_info()))
+
+
+if __name__ == "__main__":
+    main()
--- src/setting.py
+++ src/setting.py
@@ -6,6 +6,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 DISCORD_WEBHOOK = os.environ.get("DISCORD_WEBHOOK")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SERCRET = os.environ.get("CONSUMER_SERCRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SERCRET = os.environ.get("ACCESS_TOKEN_SERCRET")
